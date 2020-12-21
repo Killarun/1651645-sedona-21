635291926b28df84cee12f618a3e789a65001b27
Владимир Вергазов <vergazov@unik.local>
2020-12-21 16:59:55 +0300

    modal

--- source/js/script.js
+++ source/js/script.js
@@ -1,7 +1,9 @@
-"use strict";
+'use strict';
 
 var navMain = document.querySelector('.main-nav');
 var navToggle = document.querySelector('.main-nav__toggle');
+
+
 
 navMain.classList.remove('main-nav--nojs');
 
@@ -14,3 +16,44 @@ navToggle.addEventListener('click', function () {
     navMain.classList.remove('main-nav--opened');
   }
 });
+
+// var form = document.querySelector(".form");
+// var modalError = document.querySelector(".modal--error");
+// var modalSuccess = document.querySelector(".modal--success");
+// var modalCloseBtn;
+
+// if (form) {
+//   form.addEventListener("invalid", function () {
+//     modalError.classList.add("modal--show");
+
+//     modalCloseBtn = modalError.querySelector(".modal__close");
+//     modalCloseBtn.addEventListener("click", function (e) {
+//       e.preventDefault();
+//       modalError.classList.remove("modal--show");
+//     });
+
+//     window.addEventListener("keydown", function (e) {
+//       if (e.key === "Escape") {
+//         e.preventDefault();
+//         modalError.classList.remove("modal--show");
+//       }
+//     });
+//   }, true);
+
+//   form.addEventListener("submit", function () {
+//     modalSuccess.classList.add("modal--show");
+
+//     modalCloseBtn = modalSuccess.querySelector(".modal__close");
+//     modalCloseBtn.addEventListener("click", function (e) {
+//       e.preventDefault();
+//       modalSuccess.classList.remove("modal--show");
+//     });
+
+//     window.addEventListener("keydown", function (e) {
+//       if (e.key === "Escape") {
+//         e.preventDefault();
+//         modalSuccess.classList.remove("modal--show");
+//       }
+//     });
+//   });
+// }
